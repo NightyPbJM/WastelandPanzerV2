f758b90094c38db4400ad7e6285095fddd7c7c7e
NightyPbJM <stefan.motschenbach@googlemail.com>
2026-06-04 11:16:18 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'wasteland-panzer-v4-3';
-const FILES = ['index.html','training.html','supplements.html','quest.html','balance.html','data.html','log.html','manifest.json','icon-192.svg'];
+const CACHE = 'wasteland-panzer-v5-0';
+const FILES = ['index.html','core.js','training.html','supplements.html','quest.html','balance.html','data.html','log.html','manifest.json','icon-192.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
